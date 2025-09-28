f6b43b9e72d722d8091fe5e1c6e248425be55d4e
Peter Urbanus <purbanus@gmail.com>
2025-09-28 16:43:04 +0200

    @Test afgesterd

--- pu.chessdatabase/src/test/java/pu/chessdatabase/bo/DoBouwApart.java
+++ pu.chessdatabase/src/test/java/pu/chessdatabase/bo/DoBouwApart.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SuppressWarnings( "unused" )
 @SpringBootTest
 public class DoBouwApart
 {
 @Autowired private Bouw bouw;
-@Test
+//@Test
 public void buildDeDatabase()
 {
 	bouw.bouwDatabase();
