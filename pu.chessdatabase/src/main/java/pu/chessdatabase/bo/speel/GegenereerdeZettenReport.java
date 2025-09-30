e7994b42a6fc4016336ebbb7b83053936248b7ba
Peter Urbanus <purbanus@gmail.com>
2025-09-30 18:53:02 +0200

    Wat overbodige getters en setters weggehaald

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/speel/GegenereerdeZettenReport.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/speel/GegenereerdeZettenReport.java
@@ -19,20 +19,4 @@ public class GegenereerdeZettenReport
 private int aantalZetten;
 @Builder.Default
 private List<String> gegenereerdeZetten = new ArrayList<>(); // Liep in Modula-2 van 1 tot 130!
-public int getAantalZetten()
-{
-	return aantalZetten;
-}
-public void setAantalZetten( int aAantalZetten )
-{
-	aantalZetten = aAantalZetten;
-}
-public List<String> getGenZetten()
-{
-	return gegenereerdeZetten;
-}
-public void setGenZetten( List<String> aGenZetten )
-{
-	gegenereerdeZetten = aGenZetten;
-}
 }
