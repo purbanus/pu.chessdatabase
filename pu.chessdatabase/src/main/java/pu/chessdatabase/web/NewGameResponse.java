1b7be1e4667837d557dc02d781dd3ebe966450ed
Peter Urbanus <purbanus@gmail.com>
2025-10-03 18:18:45 +0200

    Gemaakt

---
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/web/NewGameResponse.java
@@ -0,0 +1,18 @@
+package pu.chessdatabase.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+public class NewGameResponse
+{
+private String wk;
+private String zk;
+private String s3;
+private String s4;
+private String aanZet;
+
+}
