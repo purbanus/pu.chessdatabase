76470397291b39079841bbf359cc82cb7ff0e79a
Peter Urbanus <purbanus@gmail.com>
2025-10-05 20:45:46 +0200

    Commentaartje toegevoegd

--- pu.chessdatabase/src/main/java/pu/chessdatabase/web/LoggingController.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/web/LoggingController.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Classje om logging te testen, zie https://www.baeldung.com/spring-boot-logging 
+ */
 @RestController
 public class LoggingController {
 
