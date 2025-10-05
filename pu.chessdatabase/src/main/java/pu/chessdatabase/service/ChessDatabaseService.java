e072b06e8e3bfa59c468c288c1b94b04627968eb
Peter Urbanus <purbanus@gmail.com>
2025-10-05 05:47:28 +0200

    - newGame retourneert een PartijDocument - getPartijDocument heeft nu als parm een BoStellingKey - zet() toegevoegd - toHexGetal verhuisd naar Partij

--- pu.chessdatabase/src/main/java/pu/chessdatabase/service/ChessDatabaseService.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/service/ChessDatabaseService.java
@@ -1,9 +1,12 @@
 package pu.chessdatabase.service;
 
+import pu.chessdatabase.web.GameResponse;
 import pu.chessdatabase.web.NewGameResponse;
+import pu.chessdatabase.web.ZetResponse;
 
 public interface ChessDatabaseService
 {
-public abstract void newGame( NewGameResponse newGameResponse );
-public abstract PartijDocument getPartijDocument( NewGameResponse aNewGameResponse );
+public abstract PartijDocument newGame( NewGameResponse aNewGameResponse );
+public abstract PartijDocument getPartijDocument( BoStellingKey aBoStellingKey );
+public abstract PartijDocument zet( ZetResponse aZetResponse );
 }
