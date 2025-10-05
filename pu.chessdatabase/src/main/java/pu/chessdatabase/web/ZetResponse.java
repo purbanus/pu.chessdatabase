887e0f7759e1083dd739098d08d911f8132cb639
Peter Urbanus <purbanus@gmail.com>
2025-10-05 05:51:31 +0200

    Gemaakt

---
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/web/ZetResponse.java
@@ -0,0 +1,32 @@
+package pu.chessdatabase.web;
+
+import pu.chessdatabase.bo.Kleur;
+import pu.chessdatabase.bo.speel.Partij;
+import pu.chessdatabase.service.BoStellingKey;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+public class ZetResponse
+{
+private int wk;
+private int zk;
+private int s3;
+private int s4;
+private String aanZet;
+private String nieuweZet;
+public BoStellingKey getBoStellingKey()
+{
+	return BoStellingKey.builder()
+		.wk( Partij.hexGetalToVeld( wk ) ) 
+		.zk( Partij.hexGetalToVeld( zk ) ) 
+		.s3( Partij.hexGetalToVeld( s3 ) ) 
+		.s4( Partij.hexGetalToVeld( s4 ) ) 
+		.aanZet( Kleur.fromString( aanZet ) )
+		.build();
+}
+}
