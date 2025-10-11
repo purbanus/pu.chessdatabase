0efb576690a19c6a2c5dfbc5e8dd0f2f519682aa
Peter Urbanus <purbanus@gmail.com>
2025-10-11 19:37:53 +0200

    Matrixvermenigvuldiging toegevoegd, Vector.get() gebruiken, getRowVector() toegevoegd, toString drastisch verbeterd. TestMatrix gemaakt

--- pu.chessdatabase/src/main/java/pu/chessdatabase/util/Matrix.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/util/Matrix.java
@@ -1,11 +1,14 @@
 package pu.chessdatabase.util;
 
-import lombok.ToString;
+import lombok.Data;
 
-@ToString
+@Data
 public class Matrix
 {
-public final Vector [] elements;
+/**
+ * Dit zijn de twee rijvectoren
+ */
+private final Vector [] elements;
 
 public Matrix( Vector [] aVector )
 {
@@ -15,18 +18,40 @@ public Matrix( Vector [] aVector )
 public Vector multiply( Vector aVector )
 {
 	return new Vector( 
-		getElement( 0, 0 ) * aVector.elements[0] + getElement( 0, 1) * aVector.elements[1],
-		getElement( 1, 0 ) * aVector.elements[0] + getElement( 1, 1) * aVector.elements[1]
+		getElement( 0, 0 ) * aVector.get( 0 ) + getElement( 0, 1) * aVector.get( 1 ),
+		getElement( 1, 0 ) * aVector.get( 0 ) + getElement( 1, 1) * aVector.get( 1 )
 	);
+}
+public Matrix multiply( Matrix aMatrix )
+{
+	return new Matrix(
+		new Vector [] 
+		{ 
+			new Vector( 
+				getElement( 0, 0 ) * aMatrix.getElement( 0, 0 ) + getElement( 0, 1 ) * aMatrix.getElement(  1, 0 ),
+				getElement( 0, 0 ) * aMatrix.getElement( 0, 1 ) + getElement( 0, 1 ) * aMatrix.getElement(  1, 1 )
+			),
+			new Vector( 
+				getElement( 1, 0 ) * aMatrix.getElement( 0, 0 ) + getElement( 1, 1 ) * aMatrix.getElement(  1, 0 ),
+				getElement( 1, 0 ) * aMatrix.getElement( 0, 1 ) + getElement( 1, 1 ) * aMatrix.getElement(  1, 1 )
+			),
+		}
+	);
+}
+public Vector getRowVector( int aRow )
+{
+	return getElements()[aRow];
 }
 public int getElement( int aRow, int aColumn )
 {
-	return elements[aRow].elements[aColumn];
+	return getElements()[ aRow ].get( aColumn );
 }
-//@Override
-//public String toString()
-//{
-//	return 7
-//}
+@Override
+public String toString()
+{
+	return elements[0] + "\n" + elements[1];
+//	return "[" + getElement( 0, 0 ) + " " + getElement( 0, 1 ) + "]\n" +
+//		   "[" + getElement( 1, 0 ) + " " + getElement( 1, 1 ) + "]";
+}
 
 }
