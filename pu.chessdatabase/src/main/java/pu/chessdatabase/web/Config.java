07f6aaaf9044f147b3cb7adf083a00ceb5c95a6b
Peter Urbanus <purbanus@gmail.com>
2025-10-15 20:41:43 +0200

    Toegevoegd. Dit zorgt ervoor dat Partij een sessie-object wordt

---
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/web/Config.java
@@ -0,0 +1,25 @@
+package pu.chessdatabase.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+
+import pu.chessdatabase.bo.speel.Partij;
+import pu.chessdatabase.dal.Dbs;
+
+@Configuration
+public class Config
+{
+@Autowired private Dbs dbs;
+@Bean
+@Scope( 
+	value = WebApplicationContext.SCOPE_SESSION, 
+	proxyMode = ScopedProxyMode.TARGET_CLASS )
+Partij partij()
+{
+	return new Partij( dbs );
+}
+}
