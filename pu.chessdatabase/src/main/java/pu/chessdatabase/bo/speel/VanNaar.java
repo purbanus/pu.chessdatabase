713b7279fbff39b1b44888fbf16f9c3eb6494358
Peter Urbanus <purbanus@gmail.com>
2025-10-16 22:13:42 +0200

    Niks bi

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/speel/VanNaar.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/speel/VanNaar.java
@@ -1,7 +1,6 @@
 package pu.chessdatabase.bo.speel;
 
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jdt.annotation.NonNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
