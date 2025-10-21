63ff3ccd65652213a47894898a05a560361c47b2
Peter Urbanus <purbanus@gmail.com>
2025-10-21 21:58:13 +0200

    Gemaakt

---
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/Richtingen.java
@@ -0,0 +1,15 @@
+package pu.chessdatabase.bo;
+
+import java.util.Arrays;
+import java.util.List;
+/**
+ * Gemaakt omdat de enum StukType ze niet uit Stukken kon halen, omdat ze daar niog null waren.
+ * Geen idee waarom dit wel werkt.
+ */
+public class Richtingen
+{
+public static final List<Integer> KRICHTING = Arrays.asList( 0x01, 0x11,  0x10,  0x0F, -0x01, -0x11, -0x10, -0x0F );
+public static final List<Integer> TRICHTING = Arrays.asList( 0x01, 0x10, -0x01, -0x10 );
+public static final List<Integer> LRICHTING = Arrays.asList( 0x11, 0x0F, -0x11, -0x0F );
+public static final List<Integer> PRICHTING = Arrays.asList( 0x12, 0x21,  0x1F,  0x0E, -0x12, -0x21, -0x1F, -0x0E );
+}
