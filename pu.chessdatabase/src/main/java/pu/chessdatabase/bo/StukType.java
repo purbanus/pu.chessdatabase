ddc11b3f8fc1686bf5c6d27cd64fc6c3c2ed8169
Peter Urbanus <purbanus@gmail.com>
2025-10-21 22:13:59 +0200

    richtingen en meer toegevoegd

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/StukType.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/StukType.java
@@ -1,15 +1,42 @@
 package pu.chessdatabase.bo;
 
+import java.util.List;
+
 public enum StukType
 {
-KONING( "K" ), DAME( "D" ), TOREN( "T" ), LOPER( "L" ), PAARD( "P" ); // geen pion!
+
+KONING( "K", Richtingen.KRICHTING, false ), 
+DAME( "D", Richtingen.KRICHTING, true ), 
+TOREN( "T", Richtingen.TRICHTING, true ), 
+LOPER( "L", Richtingen.LRICHTING, true ), 
+PAARD( "P", Richtingen.PRICHTING, false );
+// geen pion!
+
+//public static final List<Integer> KRICHTING = Arrays.asList( 0x01, 0x11,  0x10,  0x0F, -0x01, -0x11, -0x10, -0x0F );
+//public static final List<Integer> TRICHTING = Arrays.asList( 0x01, 0x10, -0x01, -0x10 );
+//public static final List<Integer> LRICHTING = Arrays.asList( 0x11, 0x0F, -0x11, -0x0F );
+//public static final List<Integer> PRICHTING = Arrays.asList( 0x12, 0x21,  0x1F,  0x0E, -0x12, -0x21, -0x1F, -0x0E );
+
 private String afko;
-StukType( String aAfko )
+private List<Integer> richtingen;
+private boolean meer;
+StukType( String aAfko, List<Integer> aRichtingen, boolean aMeer )
 {
+	//Richtingen richtingenVoorStukTypes = new Richtingen();
 	afko = aAfko;
+	richtingen = aRichtingen;
+	meer = aMeer;
 }
 public String getAfko()
 {
 	return afko;
+}
+public List<Integer> getRichtingen()
+{
+	return richtingen;
+}
+public boolean isMeer()
+{
+	return meer;
 }
 }
