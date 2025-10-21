3ef7f93df05ad8429704aed6f58cbb4b1ec2a8b8
Peter Urbanus <purbanus@gmail.com>
2025-10-21 22:30:21 +0200

    Doet het nog steeds niet

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/DoBouw.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/DoBouw.java
@@ -1,16 +1,27 @@
 package pu.chessdatabase.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import pu.chessdatabase.dal.Dbs;
+
+@EntityScan("pu.chessdatabase.bo")
+@SpringBootApplication 
 public class DoBouw
 {
-
+@Autowired private Bouw bouw;
+@Autowired private Dbs dbs;
+// @@NOG werkt niet! Zegt dattie Dbs bean niet kan vinden.
 public static void main( String [] args )
 {
+	SpringApplication.run( DoBouw.class, args );
 	new DoBouw().run();
 }
-
 public void run()
 {
-	Bouw bouw = new Bouw();
+	dbs.setDatabaseName( "KDKT2.DBS" );
 	bouw.bouwDatabase();
 }
 
