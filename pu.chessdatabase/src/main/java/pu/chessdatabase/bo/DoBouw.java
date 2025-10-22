52b1ddd9b1547b19b6d8998a3fc59dbe8f67db1a
Peter Urbanus <purbanus@gmail.com>
2025-10-22 04:03:06 +0200

    Afgestyerd, zorgde voor afschuwelijke fou bij tests

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/DoBouw.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/DoBouw.java
@@ -7,22 +7,22 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import pu.chessdatabase.dal.Dbs;
 
-@EntityScan("pu.chessdatabase.bo")
-@SpringBootApplication 
+//@EntityScan("pu.chessdatabase.bo")
+//@SpringBootApplication 
 public class DoBouw
 {
-@Autowired private Bouw bouw;
-@Autowired private Dbs dbs;
-// @@NOG werkt niet! Zegt dattie Dbs bean niet kan vinden.
-public static void main( String [] args )
-{
-	SpringApplication.run( DoBouw.class, args );
-	new DoBouw().run();
-}
-public void run()
-{
-	dbs.setDatabaseName( "dbs/KDKT2.DBS" );
-	bouw.bouwDatabase();
-}
-
+//@Autowired private Bouw bouw;
+//@Autowired private Dbs dbs;
+//// @@NOG werkt niet! Zegt dattie Dbs bean niet kan vinden.
+//public static void main( String [] args )
+//{
+//	SpringApplication.run( DoBouw.class, args );
+//	new DoBouw().run();
+//}
+//public void run()
+//{
+//	dbs.setDatabaseName( "dbs/KDKT2.DBS" );
+//	bouw.bouwDatabase();
+//}
+//
 }
