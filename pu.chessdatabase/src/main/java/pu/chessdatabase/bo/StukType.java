12cbde778cff18b75495907dd751cfee0faec6fa
Peter Urbanus <purbanus@gmail.com>
2025-10-23 22:26:56 +0200

    Richtingen weg hier

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/StukType.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/StukType.java
@@ -12,11 +12,6 @@ LOPER( "L", Richtingen.LRICHTING, true ),
 PAARD( "P", Richtingen.PRICHTING, false );
 // geen pion!
 
-//public static final List<Integer> KRICHTING = Arrays.asList( 0x01, 0x11,  0x10,  0x0F, -0x01, -0x11, -0x10, -0x0F );
-//public static final List<Integer> TRICHTING = Arrays.asList( 0x01, 0x10, -0x01, -0x10 );
-//public static final List<Integer> LRICHTING = Arrays.asList( 0x11, 0x0F, -0x11, -0x0F );
-//public static final List<Integer> PRICHTING = Arrays.asList( 0x12, 0x21,  0x1F,  0x0E, -0x12, -0x21, -0x1F, -0x0E );
-
 private String afko;
 private List<Integer> richtingen;
 private boolean meer;
