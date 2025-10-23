42f755ddb96901799d9c5710178622332124e5a9
Peter Urbanus <purbanus@gmail.com>
2025-10-23 22:34:52 +0200

    De configswitch netjes doen. BuildKDKT, buildKLPK en buildKLLK toegevoegd

--- pu.chessdatabase/src/test/java/pu/chessdatabase/bo/DoBouwApart.java
+++ pu.chessdatabase/src/test/java/pu/chessdatabase/bo/DoBouwApart.java
@@ -1,5 +1,7 @@
 package pu.chessdatabase.bo;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,10 +14,41 @@ public class DoBouwApart
 {
 @Autowired private Bouw bouw;
 @Autowired private Dbs dbs;
+@Autowired private Config config;
+String savedConfigString;
+@BeforeEach
+public void setup()
+{
+	savedConfigString = config.getName();
+}
+@AfterEach
+public void destroy()
+{
+	config.switchConfig( savedConfigString );
+}
+//@Test
+public void buildKDKT()
+{
+	config.switchConfig( "KDKT", false );
+	bouw.bouwDatabase();
+}
 //@Test
 public void buildKDKT2()
 {
+	config.switchConfig( "KDKT", false );
 	dbs.setDatabaseName( "dbs/KDKT2.DBS" );
+	bouw.bouwDatabase();
+}
+//@Test
+public void buildKLPK()
+{
+	config.switchConfig( "KLPK", false );
+	bouw.bouwDatabase();
+}
+//@Test
+public void buildKLLK()
+{
+	config.switchConfig( "KLLK" );
 	bouw.bouwDatabase();
 }
 }
