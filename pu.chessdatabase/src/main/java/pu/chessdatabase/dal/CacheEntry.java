943e138bea57b23ff0b3825b64146494bb5ca001
Peter Urbanus <purbanus@gmail.com>
2025-10-25 20:51:38 +0200

    Het verwijderen van overbodige @ToString en @EqualsAndHashCode en daarna massaal Organize Imports gedaan, @ToString.exclude op pageData want die is erg groot

--- pu.chessdatabase/src/main/java/pu/chessdatabase/dal/CacheEntry.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/dal/CacheEntry.java
@@ -2,16 +2,14 @@ package pu.chessdatabase.dal;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @Data
 @Builder
-@EqualsAndHashCode
-@ToString
 public class CacheEntry
 {
 private PageDescriptor pageDescriptor;
+@ToString.Exclude
 private Page page;
 private boolean vuil;
 private long generatie;
