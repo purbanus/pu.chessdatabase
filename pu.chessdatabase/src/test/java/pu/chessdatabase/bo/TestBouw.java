f04958fd15dbed8815b72b3c493435966983c10e
Peter Urbanus <purbanus@gmail.com>
2025-10-25 20:54:38 +0200

    Org imports

--- pu.chessdatabase/src/test/java/pu/chessdatabase/bo/TestBouw.java
+++ pu.chessdatabase/src/test/java/pu/chessdatabase/bo/TestBouw.java
@@ -10,9 +10,7 @@ import static pu.chessdatabase.bo.Kleur.*;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
