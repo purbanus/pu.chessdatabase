96fea411563a0abe9328ce4597d96722e15d90d1
Peter Urbanus <purbanus@gmail.com>
2025-10-28 13:42:13 +0100

    Gerenamed

--- pu.chessdatabase/src/main/java/pu/chessdatabase/dal/VMIteratorFunction.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/dal/VMIteratorFunction.java
@@ -3,7 +3,7 @@ package pu.chessdatabase.dal;
 import pu.chessdatabase.bo.BoStelling;
 
 @FunctionalInterface
-public interface VMIterateFunction
+public interface VMIteratorFunction
 {
 public abstract void doPass( BoStelling aStelling, VMStelling aVmStelling, PassFunction aPassFunction );
 }
