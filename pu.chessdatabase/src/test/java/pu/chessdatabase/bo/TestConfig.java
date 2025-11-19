f2b0101b244a162261627e63bd5c89cbce3dbedc
Peter Urbanus <purbanus@gmail.com>
2025-11-19 18:29:52 +0100

    testConfigImplRegistry toegevoegd. testSwitchConfig uitgebreid

--- pu.chessdatabase/src/test/java/pu/chessdatabase/bo/TestConfig.java
+++ pu.chessdatabase/src/test/java/pu/chessdatabase/bo/TestConfig.java
@@ -12,6 +12,7 @@ import static pu.chessdatabase.bo.configuraties.StukType.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,11 +20,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import pu.chessdatabase.bo.configuraties.ConfigImpl;
+import pu.chessdatabase.bo.configuraties.KDK;
 import pu.chessdatabase.bo.configuraties.KDKT;
+import pu.chessdatabase.bo.configuraties.KDKTT;
 import pu.chessdatabase.bo.configuraties.KLLK;
 import pu.chessdatabase.bo.configuraties.KLPK;
+import pu.chessdatabase.bo.configuraties.KTK;
 import pu.chessdatabase.bo.configuraties.StukDefinitie;
+import pu.chessdatabase.bo.configuraties.TestKDK;
 import pu.chessdatabase.bo.configuraties.TestKDKT;
+import pu.chessdatabase.dbs.VM;
 
 import lombok.Data;
 
@@ -32,6 +39,7 @@ import lombok.Data;
 public class TestConfig
 {
 @Autowired private Config config;
+@Autowired private VM vm;
 String savedConfigName;
 @BeforeEach
 public void setup()
@@ -52,13 +60,35 @@ public void testCurrentConfig()
 	assertThat( getConfig().getConfig(), is( "KDKT" ) );
 }
 @Test
+public void testGetConfigImplRegistry()
+{
+	Map<String, ConfigImpl> configImplRegistry = getConfig().getConfigImplRegistry();
+	assertThat( configImplRegistry.get( "KDK"), is( new KDK() ) );
+	assertThat( configImplRegistry.get( "KTK" ), is( new KTK() ) );
+	assertThat( configImplRegistry.get( "KDKT" ), is( new KDKT() ) );
+	assertThat( configImplRegistry.get( "KLPK" ), is( new KLPK() ) );
+	assertThat( configImplRegistry.get( "KLLK" ), is( new KLLK() ) );
+	assertThat( configImplRegistry.get( "KDKTT" ), is( new KDKTT() ) );
+	assertThat( configImplRegistry.get( "TESTKDK" ), is( new TestKDK() ) );
+	assertThat( configImplRegistry.get( "TESTKDKT" ), is( new TestKDKT() ) );
+	getConfig().switchConfig( "KLLK" );
+	assertThat( getConfig().getConfigImplRegistry().get( "KDKT" ), is( new KDKT() ) );
+	assertThat( getConfig().getConfigImplRegistry().get( "KLPK" ), is( new KLPK() ) );
+	assertThat( getConfig().getConfigImplRegistry().get( "KLLK" ), is( new KLLK() ) );
+	assertThat( getConfig().getConfigImplRegistry().get( "TESTKDKT" ), is( new TestKDKT() ) );
+}
+@Test
 public void testSwitchConfig()
 {
 	assertThrows( RuntimeException.class, () -> config.switchConfig( "PipoKoeie" ) );
-	//@@HIGH de rest
+	getConfig().switchConfig( "KLLK" );
+	assertThat( getConfig().getConfig(), is( "KLLK" ) );
+	assertThat( getConfig().getConfigImpl(), is( new KLLK() ) );
+	assertThat( getVm().getDatabaseName(), is( "dbs/KLLK.DBS" ) );
+	assertThat( getVm().isOpen(), is( true ) );
 }
 @Test
-public void testGetStukken()
+public void testGetStukList()
 {
 	getConfig().switchConfig( "klpk" );
 	List<Stuk> expectedStukken = new ArrayList<>();
@@ -98,6 +128,11 @@ public void testGetStukken()
 		.build()
 	);
 	assertThat( getConfig().getStukList(), is( expectedStukken ) );
+}
+@Test
+public void testGetStukken()
+{
+	getConfig().switchConfig( "klpk" );
 }
 @Test
 public void testgetStukDefinities()
@@ -161,6 +196,16 @@ public void testgetStukDefinities()
 	assertThat( getConfig().getStukDefinities(), is( expectedStukDefinities ) );
 }
 @Test
+public void testgetDatabaseName()
+{
+	getConfig().switchConfig( "kdk" );
+	assertThat( getConfig().getDatabaseName(), is( "dbs/KDK.DBS" ) );
+	getConfig().switchConfig( "kdkt" );
+	assertThat( getConfig().getDatabaseName(), is( "dbs/KDKT.DBS" ) );
+	getConfig().switchConfig( "kdktt", false );
+	assertThat( getConfig().getDatabaseName(), is( "dbs/KDKTT.DBS" ) );
+}
+@Test
 public void testgetAantalStukken()
 {
 	getConfig().switchConfig( "kdk" );
@@ -171,20 +216,18 @@ public void testgetAantalStukken()
 	assertThat( getConfig().getAantalStukken(), is( 5 ) );
 
 }
-@SuppressWarnings( "null" )
 @Test
-public void testGetConfigRegistry()
+public void testGetConfig()
 {
-	assertThat( getConfig().getConfigImplRegistry().get( "KDKT" ), is( new KDKT() ) );
-	assertThat( getConfig().getConfigImplRegistry().get( "KLPK" ), is( new KLPK() ) );
-	assertThat( getConfig().getConfigImplRegistry().get( "KLLK" ), is( new KLLK() ) );
-	assertThat( getConfig().getConfigImplRegistry().get( "TESTKDKT" ), is( new TestKDKT() ) );
-	getConfig().switchConfig( "KLLK" );
-	assertThat( getConfig().getConfigImplRegistry().get( "KDKT" ), is( new KDKT() ) );
-	assertThat( getConfig().getConfigImplRegistry().get( "KLPK" ), is( new KLPK() ) );
-	assertThat( getConfig().getConfigImplRegistry().get( "KLLK" ), is( new KLLK() ) );
-	assertThat( getConfig().getConfigImplRegistry().get( "TESTKDKT" ), is( new TestKDKT() ) );
+	getConfig().switchConfig( "kdk" );
+	assertThat( getConfig().getConfig(), is( "KDK" ) );
+	getConfig().switchConfig( "kdkt" );
+	assertThat( getConfig().getConfig(), is( "KDKT" ) );
+	getConfig().switchConfig( "kdktt" );
+	assertThat( getConfig().getConfig(), is( "KDKTT" ) );
 }
+
+
 //@Test
 public void testToString()
 {
