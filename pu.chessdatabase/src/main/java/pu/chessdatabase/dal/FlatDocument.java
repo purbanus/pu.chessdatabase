eb990067ddc76a2e10bf8fd1a4dd61b29fb2928e
Peter Urbanus <purbanus@gmail.com>
2026-04-13 20:53:55 +0200

    zetNummer -> plyNummer Verder plySchaak toegevoegd

--- pu.chessdatabase/src/main/java/pu/chessdatabase/dal/FlatDocument.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/dal/FlatDocument.java
@@ -18,12 +18,12 @@ private final int currentPlyNumber;
 private final boolean begonnen;
 private final int plyId;
 private final String einde;
-private final int zetNummer;
+private final int plyNummer;
 @Column( nullable = true )
 private final Integer van;
 @Column( nullable = true )
 private final Integer naar;
-private final boolean schaak;
+private final boolean plySchaak;
 private final int wk;
 private final int zk;
 private final int s3;
@@ -32,4 +32,5 @@ private final int s5;
 private final String aanZet;
 private final String resultaat;
 private final int aantalZetten;
+private final boolean schaak;
 }
