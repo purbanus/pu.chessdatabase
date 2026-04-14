240d688c83b794bb4ab6baf5b9c2f52b81a2c94f
Peter Urbanus <purbanus@gmail.com>
2026-04-14 16:27:05 +0200

    - Bij de addPly, plyNummer bepalen. Op dezelfde manier currentPlyNummer bepalen - plyNumber -> plyNummer

--- pu.chessdatabase/src/main/java/pu/chessdatabase/bo/speel/Plies.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/bo/speel/Plies.java
@@ -1,7 +1,5 @@
 package pu.chessdatabase.bo.speel;
 
-import static pu.chessdatabase.bo.Kleur.*;
-
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -119,32 +117,21 @@ public void clear()
 }
 public void addPly( Ply aPly )
 {
+	setCurrentPlyNumber( getPlies().size());
 	aPly.setPlies( this );
 	getPlies().add( aPly );
 	setBegonnen( true );
-	setVooruit();
 }
 public Ply addPly( BoStelling aBoStelling, Einde aEindeType )
 {
-	int zetNummer;
-	if ( hasPlies() )
-	{
-		zetNummer = getCurrentPly().getZetNummer();
-		if ( aBoStelling.getAanZet() == Wit )
-		{
-			zetNummer++;
-		}
-	}
-	else
-	{
-		zetNummer = 1;
-	}
+	int plyNummer;
+	plyNummer = getPlies().size();
 	Ply newPly = Ply.builder()
 		//.id is voor JPA
 		.plies( this )
 		.einde( aEindeType )
-		.zetNummer( zetNummer )
-		//.schaak @@Waarom niet??
+		.plyNummer( plyNummer )
+		//.schaak @@NOG Waarom niet??
 		//.vanNaar( VanNaar.ILLEGAL_VAN_NAAR ) // Liever null want daar kun je gemakkelijk op testen
 		.boStelling( aBoStelling )
 		.build();
