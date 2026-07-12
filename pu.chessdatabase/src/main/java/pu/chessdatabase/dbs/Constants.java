1efc1b2ddb494484df607bd3821a4f4d9820e50e
Peter Urbanus <purbanus@gmail.com>
2026-07-12 15:02:00 +0200

    Alle constantes gebruiken Pipo

--- pu.chessdatabase/src/main/java/pu/chessdatabase/dbs/Constants.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/dbs/Constants.java
@@ -2,8 +2,8 @@ package pu.chessdatabase.dbs;
 
 public interface Constants
 {
-public static final String DATABASE_NAME_PIPO = "dbs/TestPipo";
-public static final String DATABASE_NAME_PIPO4 = "dbs/TestPipo4";
-public static final String DATABASE_NAME_5PIECES = "dbs/TestPipo5";
-public static final String PREFIX_TEST_DATABASE = "dbs/Test";
+public static final String DATABASE_NAME_PIPO = "dbs/Pipo";
+public static final String DATABASE_NAME_PIPO4 = "dbs/Pipo4";
+public static final String DATABASE_NAME_5PIECES = "dbs/Pipo5";
+public static final String PREFIX_TEST_DATABASE = "dbs/Pipo";
 }
