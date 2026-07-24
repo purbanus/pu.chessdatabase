f97b0afe321d8ec6fe5a193198a519e658b63697
Peter Urbanus <purbanus@gmail.com>
2026-07-24 16:10:52 +0200

    Werd niet meer gebruikt

--- pu.chessdatabase/src/main/java/pu/chessdatabase/dbs/VMIteratorFunction.java
+++
@@ -1,9 +0,0 @@
-package pu.chessdatabase.dbs;
-
-import pu.chessdatabase.bo.BoStelling;
-
-@FunctionalInterface
-public interface VMIteratorFunction
-{
-public abstract void doPass( BoStelling aStelling, VMStelling aVmStelling, PassFunction aPassFunction );
-}
