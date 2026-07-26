0abac49df63ad1874d08693dc15200e3da09a533
Peter Urbanus <purbanus@gmail.com>
2026-07-27 00:10:34 +0200

    Dit moest ineens omdat hij geen Servletdinges meer kon vionden

--- pu.chessdatabase/src/main/java/pu/chessdatabase/Application.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/Application.java
@@ -2,12 +2,13 @@ package pu.chessdatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@EnableJpaRepositories( "pu.chessdatabase.dal" ) 
 //@EntityScan("pu.chessdatabase")
 @SpringBootApplication 
-public class Application {
-
+public class Application extends SpringBootServletInitializer
+{
 	public static void main( String[] args )
 	{
 		SpringApplication.run( Application.class, args );
