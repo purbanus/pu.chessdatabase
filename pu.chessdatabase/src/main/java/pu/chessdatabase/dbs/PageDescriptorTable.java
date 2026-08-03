dc8b418ef00ff64a834a0df530a38a7a00973a89
Peter Urbanus <purbanus@gmail.com>
2026-08-03 20:11:09 +0200

    In de create, Config gebruiken

--- pu.chessdatabase/src/main/java/pu/chessdatabase/dbs/PageDescriptorTable.java
+++ pu.chessdatabase/src/main/java/pu/chessdatabase/dbs/PageDescriptorTable.java
@@ -1,5 +1,7 @@
 package pu.chessdatabase.dbs;
 
+import pu.chessdatabase.bo.Config;
+
 // @@ Gek hoor, dat wordt duidelij hierbenede gebruikt!
 //import static pu.chessdatabase.dbs.CacheType.*;
 
@@ -10,13 +12,13 @@ public abstract void iterateOverAllPageDescriptors( PageDescriptorFunction aPage
 public abstract PageDescriptor getPageDescriptor( VMStelling aStelling );
 public abstract void setPageDescriptor( VMStelling aVmStelling, PageDescriptor aPageDescriptor );
 
-public static PageDescriptorTable create( PageSizeCalculator aPageSizeCalculator, int aAantalStukken )
+public static PageDescriptorTable create( Config aConfig )
 {
-	switch ( aPageSizeCalculator.getCacheType() )
+	switch ( aConfig.getPageSizeCalculator().getCacheType() )
 	{
-		case Serial  : return new SerialPageDescriptorTable  ( aPageSizeCalculator, aAantalStukken );
-		case Parallel: return new ParallelPageDescriptorTable( aPageSizeCalculator, aAantalStukken );
-		default: throw new RuntimeException( "Ongeldig CacheType: " + aPageSizeCalculator.getCacheType() );
+		case Serial  : return new SerialPageDescriptorTable  ( aConfig );
+		case Parallel: return new ParallelPageDescriptorTable( aConfig);
+		default: throw new RuntimeException( "Ongeldig CacheType: " + aConfig.getPageSizeCalculator().getCacheType() );
 	}
 }
 }
